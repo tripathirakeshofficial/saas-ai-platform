f12abb8612650e4cf16672854d2fab470d1e36be
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-07-19 18:02:01 +0530

    Sidebar Fixes

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,8 +5,19 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div>
       <Sheet>
