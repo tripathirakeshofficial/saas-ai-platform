3402051965b938933aa9d9dd42b5fef28b9a1b60
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-07-19 17:28:19 +0530

    Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger>
+          <Button variant="ghost" size="icon" className="md:hidden">
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0">
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default MobileSidebar;
