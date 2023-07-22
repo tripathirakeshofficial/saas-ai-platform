41d61e2ad361e7162d18cf414fd4258d174961c6
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-07-22 22:17:29 +0530

    Prismadb Error

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -6,13 +6,7 @@ declare global {
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prismadb = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
-export default prisma;
+export default prismadb;
